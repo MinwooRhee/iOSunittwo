639e52b08d705e08772488fb0f48f89bcee1b78c
Min Woo <minwoo@iMac5.local>
2019-03-04 14:14:03 -0500

    all pages finished

--- Lab - Classes.playground/Pages/2. Exercise - Create a Subclass.xcplaygroundpage/Contents.swift
+++ Lab - Classes.playground/Pages/2. Exercise - Create a Subclass.xcplaygroundpage/Contents.swift
@@ -4,7 +4,7 @@
  - Note: The exercises below are based on a game where a spaceship avoids obstacles in space. The ship is positioned at the bottom of a coordinate system and can only move left and right while obstacles "fall" from top to bottom. Throughout the exercises, you'll create classes to represent different types of spaceships that can be used in the game. The base class `Spaceship` has been provided for you below.
  */
 class Spaceship {
-    var name: String = ""
+    var name = ""
     var health = 100
     var position = 0
     
@@ -24,8 +24,8 @@ class Spaceship {
  Define a new class `Fighter` that inherits from `Spaceship`. Add a variable property `weapon` that defaults to an empty string and a variable property `remainingFirePower` that defaults to 5.
  */
 class Fighter: Spaceship {
-    var weapon: String = ""
-    var remainingFirePower: Int = 5
+    var weapon = ""
+    var remainingFirePower = 5
     
     func fire() {
         if remainingFirePower > 0 {
@@ -54,6 +54,13 @@ print("because falcon is a spaceship not a fighter")
 /*:
  Add a method to `fighter` called `fire()`. This should check to see if `remainingFirePower` is greater than 0, and if so, should decrement `remainingFirePower` by one. If `remainingFirePower` is not greater than 0, print "You have no more fire power." Call `fire()` on `destroyer` a few times and print `remainingFirePower` after each method call.
  */
-
+destroyer.fire()
+destroyer.fire()
+print(destroyer.remainingFirePower)
+destroyer.fire()
+destroyer.fire()
+destroyer.fire()
+destroyer.fire()
+print(destroyer.remainingFirePower)
 
 //: [Previous](@previous)  |  page 2 of 4  |  [Next: Exercise - Override Methods and Properties](@next)
