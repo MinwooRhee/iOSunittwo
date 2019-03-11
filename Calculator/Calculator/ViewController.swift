3ac0f5dfbf0a52fdf2190a04def46a1d1c5a0407
Min Woo <minwoo@iMac5.local>
2019-03-11 11:06:37 -0400

    Calculotor interface finished

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Min Woo on 3/8/19.
+//  Copyright © 2019 Min Woo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
