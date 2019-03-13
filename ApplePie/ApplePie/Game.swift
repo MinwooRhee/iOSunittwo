c2c3c7f4242e90fb5e94e3d89eeefb72faca5f29
Min Woo <minwoo@iMac5.local>
2019-03-13 13:48:49 -0400

    buttons disaled, Game contruct

---
+++ ApplePie/ApplePie/Game.swift
@@ -0,0 +1,14 @@
+//
+//  Game.swift
+//  ApplePie
+//
+//  Created by Min Woo on 3/13/19.
+//  Copyright © 2019 Min Woo. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
--- ApplePie/ApplePie/ViewController.swift
+++ ApplePie/ApplePie/ViewController.swift
@@ -9,15 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var listOfWords = ["apple", "kimchi", "meat", "beethoven"]
+    let incorrectMovesAllowed = 7
+    var totalWins = 0
+    var totalLosses = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        newRound()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    var currentGame: Game!
+    
+    func newRound() {
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
+    }
 
     @IBOutlet weak var treeImageView: UIImageView!
     @IBOutlet weak var correctWordLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
 }
 
