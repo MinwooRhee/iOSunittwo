47778373eb7c06249505bc04ef1abbe9f3743d87
Min Woo <minwoo@iMac5.local>
2019-04-01 08:59:50 -0400

    Applepie almost finished

--- ApplePie/ApplePie/ViewController.swift
+++ ApplePie/ApplePie/ViewController.swift
@@ -12,8 +12,16 @@ class ViewController: UIViewController {
     
     var listOfWords = ["apple", "kimchi", "meat", "beethoven"]
     let incorrectMovesAllowed = 7
-    var totalWins = 0
-    var totalLosses = 0
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +38,12 @@ class ViewController: UIViewController {
     }
     
     func updateUI() {
+        var letters = [String]()
+        for letter in currentGame.formattedWord {
+            letters.append(String(letter))
+        }
+        let wordWithSpacing = letters.joined(separator: " ")
+        correctWordLabel.text = wordWithSpacing
         scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
         treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
     }
@@ -37,11 +51,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var treeImageView: UIImageView!
     @IBOutlet weak var correctWordLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
+    
     @IBAction func buttonTapped(_ sender: UIButton) {
         sender.isEnabled = false
         let letterString = sender.title(for: .normal)!
         let letter = Character(letterString.lowercased())
         currentGame.playerGuessed(letter: letter)
-        updateUI()
+        updateGameState()
     }
+    
+    func updateGameState() {
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        }
+        else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        }
+        else {
+            updateUI()
+        }
+    }
+    
 }
