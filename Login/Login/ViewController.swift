1c55a4edb601f1302246e8baf24931939632b24b
Min Woo <minwoo@iMac5.local>
2019-04-05 14:14:39 -0400

    view and navigarion controller

---
+++ Login/Login/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Min Woo on 4/5/19.
+//  Copyright © 2019 Min Woo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+
+
+}
+
