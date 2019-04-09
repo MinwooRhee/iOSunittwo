8b0c453e9636717f0b4d2ddf5b1890615e43ec96
Min Woo <minwoo@iMac5.local>
2019-04-09 15:11:49 -0400

    forgot username and password segueways

--- Login/Login/ViewController.swift
+++ Login/Login/ViewController.swift
@@ -16,11 +16,27 @@ class ViewController: UIViewController {
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        segue.destination.title = userName.text
+        guard let sender = sender as? UIButton else {return}
         
+        if sender == forgotUserNameButton {
+            segue.destination.navigationItem.title = "Forgot User Name"
+        }
+        else if sender == forgotPasswordButton {
+            segue.destination.navigationItem.title = "Forgot Password"
+        }
+        else {
+            segue.destination.title = userName.text
+        }
     }
     
     @IBOutlet weak var userName: UITextField!
-
+    @IBOutlet weak var forgotUserNameButton: UIButton!
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    
+    @IBAction func forgotUserName(_ sender: Any) {
+        performSegue(withIdentifier: "forgot", sender: forgotUserNameButton)
+    }
+    @IBAction func forgotPassword(_ sender: Any) {
+        performSegue(withIdentifier: "forgot", sender: forgotPasswordButton)
+    }
 }
-
