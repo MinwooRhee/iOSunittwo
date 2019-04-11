157fe1f4a512f49551f3aee02bd785d0b4bea6a1
Min Woo <minwoo@iMac5.local>
2019-04-11 08:56:48 -0400

    tab bar controller and views

---
+++ About Me/About Me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  About Me
+//
+//  Created by Min Woo on 4/11/19.
+//  Copyright © 2019 Min Woo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
