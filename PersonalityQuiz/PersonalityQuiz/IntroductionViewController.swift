8aeabc5197df5e9ff203ccb71e2218b14019dc12
Min Woo <minwoo@iMac5.local>
2019-04-18 15:08:42 -0400

    segueways and navigation controller and classes

---
+++ PersonalityQuiz/PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Min Woo on 4/16/19.
+//  Copyright © 2019 Min Woo. All rights reserved.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
