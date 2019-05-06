4f1901bb1d09c4b6cc5564f56df52241739aebed
Min Woo <minwoo@iMac5.local>
2019-05-06 12:04:33 -0400

    Emoji Dictionary statrted

---
+++ EmojiDictionary2/EmojiDictionary2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiDictionary2
+//
+//  Created by Min Woo on 5/6/19.
+//  Copyright © 2019 Min Woo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
